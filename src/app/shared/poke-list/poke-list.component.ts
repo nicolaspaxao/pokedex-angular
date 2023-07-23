860168003da9bb37c82937c279bb5dc2dffe2fdb
Nicolas Paxão da Silva <89174235+nicolaspaxao@users.noreply.github.com>
2023-07-22 22:27:07 -0300

    adicionando a tela de detalhes com animação

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,15 +7,28 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit {
+  private setPokeList: any;
   public pokeList: any;
+  public apiError: boolean = false;
 
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe({
-      next: (res => { this.pokeList = res.results; }),
-      error: (error) => { alert(error) }
-    });
+    if (this.setPokeList == null)
+      this.pokeApiService.apiListAllPokemons.subscribe({
+        next: (res => {
+          this.setPokeList = res.results;
+          this.pokeList = this.setPokeList;
+        }),
+        error: (error) => { this.apiError = true }
+      });
   }
 
+  public search(event: any) {
+    const filter = this.setPokeList.filter((res: any) => {
+      return !res.name.indexOf(event.toLowerCase())
+    });
+
+    this.pokeList = filter;
+  }
 }
--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./poke-search.component.scss']
 })
 export class PokeSearchComponent {
+  @Output() public emmitSearch = new EventEmitter();
 
+  constructor() { }
+
+  public search(val: string) {
+    this.emmitSearch.emit(val);
+  }
 }
